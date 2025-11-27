4c77ae0dd251bc285cc84962af8632e8c4a5b1e2
TKTTECH1 <tafadzwakureya799@gmail.com>
2025-11-27 12:57:32 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# SD-MD-WHATSAPP-BOT
+
+<p align="center">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset=".media/https://files.catbox.moe/lfne89.png">
+    <img alt="TKT-CYBER_MD" src=".media/https://files.catbox.moe/lfne89.png">
+  </picture>
+</p>
+
+A powerful Whatsapp bot that based on baileys.
---
+++ session.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "SD-MD~vQNZABJ6kmekMUU",
+PORT: process.env.PORT || "8000"
+};
