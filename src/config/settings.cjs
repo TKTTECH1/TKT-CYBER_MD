4c54f19a1832d441db71a978be0959f05fab9d5f
TKTTECH1 <tafadzwakureya799@gmail.com>
2025-11-27 13:01:07 +0200

    Add files via upload

---
+++ src/config/settings.cjs
@@ -0,0 +1,39 @@
+
+module.exports = {
+  "PREFIX": ".",
+  "OWNERS": [
+    "263718095555@s.whatsapp.net"
+  ],
+  "PORT": "3000",
+  "AUTOREADSTATUS": true,
+  "MODE": "public",
+  "BOTNUMBER": "263718095555",
+  "OMDBAPI_KEY": "76cb7f39",
+  "READCMD": true,
+  "AUTOVOICE": false,
+  "AUTOSTICKER": true,
+  "AUTOREPLY": true,
+  "AUTOREACT": false,
+  "WELCOME": false,
+  "ANTIBAD": true,
+  "ANTIBOT": true,
+  "ANTILINK": false,
+  "ANTICALL": false,
+  "ANTIDELETE": true,
+  "ALLWAYSONLINE": true,
+  "MOROCCOBLOCK": true,
+  "OWNERNAME": "TAFADZWA-TKT",
+  "NEWSJID": "120363418027651738@newsletter",
+  "MOVIEJID": false,
+  "ANIMEJID": "120363418027651738@newsletter",
+  "AUTOTYPING": false,
+  "AUTORECORDING": true,
+  "AUTONEWS": false,
+  "OWNERREACT": false,
+  "EMOJI": "🇿🇼",
+  "AIMODE": false,
+  "BANNED": [],
+  "SUDO": [
+    "263781400473"
+  ]
+};
